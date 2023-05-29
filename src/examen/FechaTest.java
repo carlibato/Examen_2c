18fe63f371e005b792df8c2d564ded1f17c2972b
“carlibato” <carlibato@gmail.com>
2023-05-29 12:25:00 +0200

    Proyecto sin documentar

---
+++ src/examen/FechaTest.java
@@ -0,0 +1,17 @@
+package examen;
+
+public class FechaTest {
+	
+public void test() {
+	assertFalse.FechaT(dia=-1);
+}
+public void test1() {
+	assertFalse.FechaTest(mes=-4);
+}
+public void test2() {
+	assertFalse.FechaTest(anyo=-1);
+}
+public void test3() {
+	assertFalse.FechaTest(mes=22);
+}
+}
